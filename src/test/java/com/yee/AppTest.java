5848a3358d3c290a38324adcc50d05a21862df06
rogeryee <roger.yee@hotmail.com>
2015-07-02 13:35:03 +0800

    Add test

---
+++ src/test/java/com/yee/AppTest.java
@@ -0,0 +1,15 @@
+package com.yee;
+
+import org.junit.Test;
+
+/**
+ * Author: RogerYee
+ */
+public class AppTest
+{
+    @Test
+    public void testGetApp()
+    {
+        System.out.println("test app");
+    }
+}
